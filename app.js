621d60ae5b4d35574bf626b42c279de6b4fc4078
sangsangga <wandaputrasangga@gmail.com>
2020-11-20 07:36:01 +0700

    add heroku config

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const router = require('./routes/index.js')
 const session = require('express-session')
 
